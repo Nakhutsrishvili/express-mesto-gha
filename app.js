e96c02ffcfddfb569d4728badf71a8919f49cf98
Nodari Nakhutsrishvili <Nakhutsrishvili@yandex.ru>
2023-08-14 08:40:09 +0300

    express-mesto-gha-13pr

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/testdb' } = process.env;
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64d8dff78f2575429c165063',
+  };
+  next();
+});
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
+
+app.listen(PORT);
