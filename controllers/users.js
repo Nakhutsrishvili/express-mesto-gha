12f132940616eea84bbb793b1bae5d58ee4c76c2
Nodari Nakhutsrishvili <Nakhutsrishvili@yandex.ru>
2023-08-14 09:04:29 +0300

    express-mesto-gha-13pr

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(users))
+    .then((users) => res.send(users))
     .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
 };
 
